46159a26d78badbd9edb4367f5557b1a76885d37
Ian <iandonghun@gmail.com>
2020-06-08 17:28:53 +0900

    hello world

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-var express = require('express');
-var app = express();
+const express = require('express'); // 설치한 express module 콜
+const app = express(); // express 실행 및 app object 초기화
 
-app.get('/', function(req, res) {
+// '\'위치에 get 요청을 받으면 'Hello World!' 보내기
+app.get('/', function(req, res){
   res.send('Hello World!');
 });
 
-var port = 3000;
+//사용할 포트 번호 3000
+const port = 3000;
+
+//서버가 실행되는 경우 콘솔창에 띄우기
 app.listen(port, function(){
-  console.log('server on! http://localhost:'+port);
+  console.log('서버가 열렸습니다!!! http://localhost:'+ port);
 });
